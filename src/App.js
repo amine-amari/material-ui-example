bf599278511b604c6d4d332e34f8665e2c8de11b
Amine Amari <amari.mohamed.amine90@gmail.com>
2023-01-09 18:39:35 +0100

    add cards and styling

--- src/App.js
+++ src/App.js
@@ -2,28 +2,30 @@ import './App.css';
 import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, Button} from '@mui/material';
 import { PhotoCamera } from '@mui/icons-material';
 
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
 function App() {
   return (
     <>
       <CssBaseline />
       <AppBar position='relative'>
         <Toolbar>
-          <PhotoCamera />
+          <PhotoCamera className='photo_icon'/>
           <Typography variant='h6'>
             Photo Album
           </Typography>
         </Toolbar>
       </AppBar>
       <main>
-        <div>
-          <Container maxWidth='sm'>
+        <div className='main_container'>
+          <Container maxWidth='sm' >
             <Typography variant='h2' align='center' color='textPrimary' gutterBottom>
               Photo Album
             </Typography>
             <Typography variant='h5' align='center' color='textSecondary' paragraph>
               This is a photo album and this a long sentence to showcase how it will be displayed on the page
             </Typography>
-            <div>
+            <div className='button_grid'>
               <Grid container spacing={2} justifyContent='center'>
                 <Grid item>
                   <Button variant='contained' color='primary'>
@@ -39,6 +41,33 @@ function App() {
             </div>
           </Container>
         </div>
+        <Container className='card_grid' maxWidth='md'>
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card className='card'>
+                  <CardMedia 
+                    className='card_media'
+                    image='https://source.unsplash.com/random'
+                    title= 'Image title'
+                  />
+                  <CardContent className='card_content'>
+                    <Typography gutterBottom variant='h5'>
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can use this section to describe the content.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size='small' color='primary'>View</Button>
+                    <Button size='small' color='primary'>Edit</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
       </main>
     </>
   );
